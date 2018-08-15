8b82be20d60a9cf1f6487091aa785ccffb4309d1
dOkI <denis.kirpichenkov@gmail.com>
2018-08-16 00:42:10 +0500

    Attach API updated

--- src/main/java/com/focusit/openj9stack.java
+++ src/main/java/com/focusit/openj9stack.java
@@ -1,22 +1,24 @@
 package com.focusit;
 
 import com.ibm.tools.attach.attacher.OpenJ9VirtualMachine;
-import com.ibm.tools.attach.target.diagnostics.ThreadDumpItem;
-import com.ibm.tools.attach.target.diagnostics.ThreadInfoSerializable;
+import com.ibm.tools.attach.target.diagnostics.data.ClassInfoResponse;
+import com.ibm.tools.attach.target.diagnostics.data.CompilationInfoResponse;
+import com.ibm.tools.attach.target.diagnostics.data.GCInfoResponse;
+import com.ibm.tools.attach.target.diagnostics.data.ThreadInfoResponse;
 import com.sun.tools.attach.AttachNotSupportedException;
 
 import java.io.IOException;
-import java.lang.management.ThreadInfo;
 
 public class openj9stack {
 
     public static void main(String[] args) throws IOException, AttachNotSupportedException, ClassNotFoundException {
         System.out.println("OpenJ9 diagnostic utility");
-        OpenJ9VirtualMachine vm = (OpenJ9VirtualMachine) OpenJ9VirtualMachine.attach("25330");
-
-        String pathToDump = vm.triggerJavaDump("");
-        System.out.println("Dumpled to "+pathToDump);
-        ThreadInfoSerializable items[] = vm.getThreadDump(true, true);
-        System.out.println("Threads: "+items.length);
+        OpenJ9VirtualMachine vm = (OpenJ9VirtualMachine) OpenJ9VirtualMachine.attach("18437");
+        ThreadInfoResponse info[] = vm.getThreadDump(true, true);
+        CompilationInfoResponse compilationInfo = vm.getCompilationInfo();
+        ClassInfoResponse classInfo = vm.getClassInfo();
+        GCInfoResponse gcInfo = vm.getGCInfo();
+        vm.detach();
+        System.out.println(info.length);
     }
 }
