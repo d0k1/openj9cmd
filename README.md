f268b17f363c8750713c087996e82492cd75dd3f
dOkI <denis.kirpichenkov@gmail.com>
2018-08-18 01:30:31 +0500

    ReleaseCandidate1

--- README.md
+++ README.md
@@ -3,12 +3,20 @@ OpenJ9 doesn't have any command line diagnostic utilities like jstack / jmap / j
 It is because OpenJ9 does not have Attach API extensions as OpenJDK have at the moment.
 This project demonstrates abilities of Attach API in OpenJ9 that are added with https://github.com/eclipse/openj9/issues/2543
 
-## Example
+## Build
+You need Eclipse OpenJ9 JDK and Apache Maven.
+
+* Define environmental variable `JAVA_HOME`. It must point to OpenJ9 jdk directory.
+* Run `mvn clean package`
+That's it. `target` directory will contains `openj9cmd-1.0.jar`
+
+
+## Prepare
 
 You cant wrap this tool into shell script, for example openj9cmd.sh
 
 `
-${JAVA_HOME}/bin/java -classpath ${JAVA_HOME}/jre/lib/charsets.jar:${JAVA_HOME}/jre/lib/ext/cldrdata.jar:${JAVA_HOME}/jre/lib/ext/dnsns.jar:${JAVA_HOME}/jre/lib/ext/dtfj.jar:${JAVA_HOME}/jre/lib/ext/dtfjview.jar:${JAVA_HOME}/jre/lib/ext/jaccess.jar:${JAVA_HOME}/jre/lib/ext/localedata.jar:${JAVA_HOME}/jre/lib/ext/nashorn.jar:${JAVA_HOME}/jre/lib/ext/sunec.jar:${JAVA_HOME}/jre/lib/ext/sunjce_provider.jar:${JAVA_HOME}/jre/lib/ext/sunpkcs11.jar:${JAVA_HOME}/jre/lib/ext/traceformat.jar:${JAVA_HOME}/jre/lib/ext/zipfs.jar:${JAVA_HOME}/jre/lib/jce.jar:${JAVA_HOME}/jre/lib/jsse.jar:${JAVA_HOME}/jre/lib/management-agent.jar:${JAVA_HOME}/jre/lib/resources.jar:${JAVA_HOME}/jre/lib/rt.jar:${JAVA_HOME}/lib/tools.jar:./target/openj9cmd-1.0-SNAPSHOT.jar com.focusit.openj9cmd "$@"
+${JAVA_HOME}/bin/java -classpath ${JAVA_HOME}/jre/lib/charsets.jar:${JAVA_HOME}/jre/lib/ext/cldrdata.jar:${JAVA_HOME}/jre/lib/ext/dnsns.jar:${JAVA_HOME}/jre/lib/ext/dtfj.jar:${JAVA_HOME}/jre/lib/ext/dtfjview.jar:${JAVA_HOME}/jre/lib/ext/jaccess.jar:${JAVA_HOME}/jre/lib/ext/localedata.jar:${JAVA_HOME}/jre/lib/ext/nashorn.jar:${JAVA_HOME}/jre/lib/ext/sunec.jar:${JAVA_HOME}/jre/lib/ext/sunjce_provider.jar:${JAVA_HOME}/jre/lib/ext/sunpkcs11.jar:${JAVA_HOME}/jre/lib/ext/traceformat.jar:${JAVA_HOME}/jre/lib/ext/zipfs.jar:${JAVA_HOME}/jre/lib/jce.jar:${JAVA_HOME}/jre/lib/jsse.jar:${JAVA_HOME}/jre/lib/management-agent.jar:${JAVA_HOME}/jre/lib/resources.jar:${JAVA_HOME}/jre/lib/rt.jar:${JAVA_HOME}/lib/tools.jar:./target/openj9cmd-1.0.jar com.focusit.openj9cmd "$@"
 `
 
 later you can call it
@@ -16,3 +24,27 @@ later you can call it
 bash ./openj9cmd.sh 31605 threaddump /tmp/threaddump
 bash ./openj9cmd.sh 31605 jstack true true jstack.txt
 ```
+
+## Run
+
+This tool provides just 3 basic functions. It can trigger OpenJ9 JavaDump and HeapDump, and it can get thread dump in jstack fashion.
+
+### JavaDump
+`bash ./openj9cmd.sh PID threaddump PATH/TO/FILE`
+
+- PID processId
+- PATH/TO/FILE file to hold all information
+
+### HeapDump
+`bash ./openj9cmd.sh PID heapdump PATH/TO/FILE`
+
+- PID processId
+- PATH/TO/FILE file to hold all information
+
+### JStack
+`bash ./openj9cmd.sh PID jstack LockeMonitors LockedSynchronizers PATH/TO/FILE`
+
+- PID processId
+- LockeMonitors whether or not information on all currently locked object monitors is to be returned (as for ThreadMXBean's JavaDoc)
+- LockedSynchronizers of whether or not information on all currently locked ownable synchronizers is to be returned (as for ThreadMXBean's JavaDoc)
+- PATH/TO/FILE file to hold all information
--- src/main/java/com/focusit/openj9cmd.java
+++ src/main/java/com/focusit/openj9cmd.java
@@ -1,15 +1,20 @@
 package com.focusit;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import com.ibm.tools.attach.attacher.OpenJ9VirtualMachine;
 import com.ibm.tools.attach.target.diagnostics.data.ThreadInfoResponse;
 import com.sun.tools.attach.AttachNotSupportedException;
 
-import java.io.IOException;
-
 public class openj9cmd {
 
     public static void main(String[] args) throws IOException, AttachNotSupportedException, ClassNotFoundException {
-        System.out.println("OpenJ9 diagnostic utility");
+        System.out.println("Eclipse OpenJ9 diagnostic utility");
         OpenJ9VirtualMachine vm = (OpenJ9VirtualMachine) OpenJ9VirtualMachine.attach(args[0]);
 
         switch (args[1]) {
@@ -26,14 +31,59 @@ public class openj9cmd {
             }
 
             case "jstack": {
-                String filename = args[4];
+            String filename = "";
+            if (args.length > 4)
+            {
+                filename = args[4];
+            }
                 ThreadInfoResponse infos[] = vm.getThreadDump(Boolean.parseBoolean(args[2]), Boolean.parseBoolean(args[3]));
-//                PrintWriter writer = new PrintWriter(System.out);
-//
-//                for(ThreadInfoResponse info:infos){
-//
-//                }
+            PrintWriter writer = new PrintWriter(System.out);
+            if (!filename.isEmpty())
+            {
+                writer = new PrintWriter(new FileOutputStream(new File(filename)));
+                writer.println("Eclipse OpenJ9 diagnostic utility");
+            }
+            writer.println("Full thread dump");
+            writer.println(new SimpleDateFormat().format(new Date()));
 
+            writer.println();
+
+            for (ThreadInfoResponse info : infos)
+            {
+                writer.printf("\"%s\" #%d %s prio=? os_prio=? tid=? nid=%d %s %s\n", info.getThreadName(),
+                        info.getThreadId(), "daemon?", info.getNativeTId(),
+                        Integer.toHexString((int)info.getNativeTId()),
+                        info.getLockName() == null ? "" : "waiting " + info.getLockName());
+                writer.printf("   java.lang.ThreadState: %s\n", info.getThreadState());
+
+                for (StackTraceElement item : info.getStackTraces())
+                {
+                    writer.printf("\tat %s.%s(%s:%d)\n", item.getClassName(), item.getMethodName(), item.getFileName(),
+                            item.getLineNumber());
+                }
+
+                writer.println("   Locked monitors:");
+
+                for (ThreadInfoResponse.LockInfoResponse lock : info.getLockedMonitors())
+                {
+                    writer.printf("      - %s@%s\n", lock.getClassName(),
+                            Integer.toHexString(lock.getIdentityHashCode()));
+
+                }
+
+                writer.println();
+                writer.println("   Locked synchronizers:");
+
+                for (ThreadInfoResponse.LockInfoResponse lock : info.getLockedSynchronizers())
+                {
+                    writer.printf("      - %s@%s\n", lock.getClassName(),
+                            Integer.toHexString(lock.getIdentityHashCode()));
+
+                }
+                writer.println();
+                writer.println();
+            }
+            writer.flush();
                 break;
             }
         }
